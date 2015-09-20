588f98aca051f94b5b160bdd7c0a2edb9565d05e
Matias Nahuel Heredia <matias@informaticos.com>
2015-09-20 14:30:17 -0300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angApp
+ */
+angular.module('angApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
